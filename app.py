c3ca4d12a64e6e61093ccd641887d62d4a9ee5f6
Lanin Chowdhury <laninchowdhuryva@gmail.com>
2024-10-11 03:48:57 -0400

    add app.py as the main application file

--- app.py
+++ app.py
@@ -8,3 +8,20 @@ import os
 df = pd.read_csv("C:\\Users\\nsuka\\Downloads\\vehicles_us.csv")
 df.head()
 
+# Add a header
+st.header("Car Listings Analysis")
+
+# create a plotly express histogram
+fig = px.histogram(df, x='price', title='Price Distribution')
+st.plotly_chart(fig)
+
+# st.write(fig_hist)
+
+show_only_4wd = st.checkbox("Show only 4WD vehicles")
+
+if show_only_4wd:
+    df = df[df['is_4wd'] == 1.0]
+
+
+fig_hist = px.scatter(df, x='odometer', y='price', color='condition', title='Price vs. Odometer by Condition')
+st.write(fig_hist)
