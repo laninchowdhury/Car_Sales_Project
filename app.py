3e40bdbf69790b7d716ba196d65637d42af26997
Lanin Chowdhury <laninchowdhuryva@gmail.com>
2024-10-11 03:31:20 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import altair as alt
+import os
+
+
+df = pd.read_csv("C:\\Users\\nsuka\\Downloads\\vehicles_us.csv")
+df.head()
+
